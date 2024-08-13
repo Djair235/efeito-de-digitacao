4bfc5b5c66899a7a9eaa5345d9ed4cbb0fc1a472
djair.fts <140333747+Djair235@users.noreply.github.com>
2024-08-13 20:01:11 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-const body = document.body;
-const button = document.getElementById("button");
-const p = document.getElementById("p1");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("bodyCorTrocada");
-    p.classList.toggle("pCorTrocada")
-    button.classList.toggle("buttonCorTrocada");
-}); 
